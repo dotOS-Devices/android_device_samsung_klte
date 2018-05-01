7c6df7cefcc93b406d166518140e155a8e6c8351
Bruno <37053961+brunowp8@users.noreply.github.com>
2018-05-01 03:12:42 -0300

    DOT-ify

--- dot.mk
+++ dot.mk
@@ -18,14 +18,14 @@
 $(call inherit-product, device/samsung/klte/full_klte.mk)
 
 # Inherit some common aosp stuff.
-$(call inherit-product, vendor/aosp/common.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Include Bootanimation configuration
 TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := klte
-PRODUCT_NAME := aosp_klte
+PRODUCT_NAME := dot_klte
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
 DEVICE_MAINTAINERS="Brunowp8" \
